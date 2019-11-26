09585aae89c216ac17466b7e7e991f47175c4ee8
Mehmet Yener YILMAZ <yilmazmehmetyener@gmail.com>
2019-11-26 04:07:40 +0300

    initial

---
+++ App.js
@@ -0,0 +1,239 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  AsyncStorage,
+  Alert
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import firebase from 'react-native-firebase';
+
+class App extends React.Component {
+
+  async componentDidMount() {
+    this.checkPermission();
+    this.createNotificationListeners(); //add this line
+  }
+
+  componentWillUnmount() {
+    this.notificationListener;
+    this.notificationOpenedListener;
+  }
+
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      this.getToken();
+    } else {
+      this.requestPermission();
+    }
+  }
+
+  async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      this.getToken();
+    } catch (error) {
+      // User has rejected permissions
+      console.log('permission rejected');
+    }
+  }
+
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        // user has a device token
+        console.log('fcmToken:', fcmToken);
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    }
+    console.log('fcmToken:', fcmToken);
+  }
+
+  async createNotificationListeners() {
+    /*
+    * Triggered when a particular notification has been received in foreground
+    * */
+    this.notificationListener = firebase.notifications().onNotification((notification) => {
+      const { title, body } = notification;
+      console.log('onNotification:');
+
+      const localNotification = new firebase.notifications.Notification({
+        sound: 'sampleaudio',
+        show_in_foreground: true,
+      })
+        .setSound('sampleaudio.mp3')
+        .setNotificationId(notification.notificationId)
+        .setTitle(notification.title)
+        .setBody(notification.body)
+        .android.setChannelId('fcm_pushnotification_default_channel') // e.g. the id you chose above
+        .android.setSmallIcon('@drawable/ic_launcher') // create this icon in Android Studio
+        .android.setColor('#000000') // you can set a color here
+        .android.setPriority(firebase.notifications.Android.Priority.High);
+
+      firebase.notifications()
+        .displayNotification(localNotification)
+        .catch(err => console.error(err));
+    });
+
+    const channel = new firebase.notifications.Android.Channel('fcm_pushnotification_default_channel', 'Demo app name', firebase.notifications.Android.Importance.High)
+      .setDescription('Demo app description')
+      .setSound('sampleaudio.mp3');
+    firebase.notifications().android.createChannel(channel);
+
+    /*
+    * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+    * */
+    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+      const { title, body } = notificationOpen.notification;
+      console.log('onNotificationOpened:');
+      debugger
+      Alert.alert(title, body)
+    });
+
+
+    /*
+    * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+    * */
+    const notificationOpen = await firebase.notifications().getInitialNotification();
+    debugger
+    if (notificationOpen) {
+      const { title, body } = notificationOpen.notification;
+      const { data } = notificationOpen.notification;
+      console.log('getInitialNotification:');
+      Alert.alert(title, body)
+    }
+    /*
+    * Triggered for data only payload in foreground
+    * */
+    this.messageListener = firebase.messaging().onMessage((message) => {
+      //process data message
+      debugger
+      console.log("JSON.stringify:", JSON.stringify(message));
+    });
+  }
+
+
+  async componentDidMount() {
+    this.checkPermission();
+    this.createNotificationListeners(); //add this line
+  }
+
+  componentWillUnmount() {
+    this.notificationListener;
+    this.notificationOpenedListener;
+  }
+
+  render() {
+
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <Header />
+            {global.HermesInternal == null ? null : (
+              <View style={styles.engine}>
+                <Text style={styles.footer}>Engine: Hermes</Text>
+              </View>
+            )}
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step One</Text>
+                <Text style={styles.sectionDescription}>
+                  Edit <Text style={styles.highlight}>App.js</Text> to change this
+                  screen and then come back to see your edits.
+              </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>See Your Changes</Text>
+                <Text style={styles.sectionDescription}>
+                  <ReloadInstructions />
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Debug</Text>
+                <Text style={styles.sectionDescription}>
+                  <DebugInstructions />
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Learn More</Text>
+                <Text style={styles.sectionDescription}>
+                  Read the docs to discover what to do next:
+              </Text>
+              </View>
+              <LearnMoreLinks />
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
---
+++ bgMessaging.js
@@ -0,0 +1,9 @@
+// @flow
+import firebase from 'react-native-firebase';
+// Optional flow type
+import type  from 'react-native-firebase';
+
+export default async (message) => {
+    // handle your message
+    return Promise.resolve();
+}
